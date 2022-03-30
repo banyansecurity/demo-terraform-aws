112f48db7553a36b68a36426f05a47cd221665c3
Tarun Desikan <tarun@banyansecurity.io>
2022-03-30 16:22:13 -0700

    connector - initial files

---
+++ 4-banyan-connector/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+module "aws_connector" {
+  source                 = "../terraform-aws-banyan-connector"
+  vpc_id                 = "vpc-123"
+  subnet_id              = "subnet-123"
+  ssh_key_name           = "my-key"
+  api_key_secret         = "abc123..."
+  connector_name         = "my-banyan-connector"
+}
