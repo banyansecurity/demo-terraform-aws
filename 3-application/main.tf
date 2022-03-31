3630cbeeb296d77d77ec2bec08e5d0507d728603
Carlos Martinez <carlos@banyansecurity.io>
2022-03-30 23:26:42 -0700

    publish 3-application

---
+++ 3-application/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+module "network" {
+  source = "../1-network"
+}
+
+provider "aws" {
+  region  = "${module.network.region}"
+  profile  = "${module.network.profile}"
+
+}
+
+resource "aws_instance" "devops_demo" {
+  ami           = "ami-00ee4df451840fa9d"
+  instance_type = "t2.micro"
+  key_name = "WorkshopKeyPair"
+  subnet_id		= "${module.network.private_subnet}"
+  vpc_security_group_ids	= "${module.network.private_network_security_group}"
+  associate_public_ip_address = false
+  
+  tags = {
+    Name = "workshop-appsrvr1"
+  }
+}
